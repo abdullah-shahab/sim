d358c75a8483c20a27ee9f70f480d975481bea02
SHAZADA §HARIAR <68187675+shazada-shariar@users.noreply.github.com>
2021-02-13 21:06:36 +0430

    Add files via upload

---
+++ sim.sh
@@ -0,0 +1,301 @@
+#!/bin/bash
+
+
+
+clear
+clear
+apt update
+apt upgrade
+apt install figlet -y
+figlet Basic Installation
+apt install toilet -y
+apt install cowsay -y
+apt install nano -y
+apt install ruby -y
+gem install lolcat
+apt install git curl openssh openssl openssl-tool -y
+
+figlet -f big Farzad !!! | lolcat
+
+sleep 5
+
+
+
+figlet -f big KING !!! | lolcat
+
+sleep 2
+
+
+figlet -f big Termux !!! | lolcat
+
+
+sleep 2
+
+
+figlet -f big Commands !!! | lolcat
+
+
+sleep 5
+
+
+figlet -f big credeit hack !!! | lolcat
+
+
+sleep 4
+
+
+figlet -f big  Afg  !!! | lolcat
+
+sleep 2
+
+figlet -f big  sim  !!! | lolcat
+
+sleep 1
+
+figlet -f big  Farzad  !!! | lolcat
+
+sleep 1
+
+
+
+echo " Careted by Farzad Jan " | lolcat
+
+sleep 1
+
+echo " TERMUX COMMANDS CHINAL " | lolcat
+
+sleep 1
+
+echo " Hack sim card credit careted by Fazad admin in Telegram chinal Termux Commands use evryweary our chinal plese select the number to hack credit and dont shere this script becase the network so not be worked and the siestem spam serch every day to faind the bug " | lolcat
+
+sleep 7
+
+echo " plese selsect number  " | lolcat
+
+echo " Note: Only select Number in 10 sec dont enter "
+
+sleep 10
+
+echo " [1] etsalat " | lolcat
+echo " [2] MTN " | lolcat
+echo " [3] Awcc " | lolcat
+echo " [4] Salaam " | lolcat
+echo " [5] Roshan " | lolcat
+
+sleep 5
+echo " Plese with... " | lolcat
+sleep 5
+clear
+
+
+
+echo " ● ● ● " | lolcat
+sleep 1
+clear
+echo " ■ ■ ■" | lolcat
+sleep 1
+clear
+echo "● ● ●" |lolcat
+
+sleep 2
+clear
+figlet -f big  Farzad | lolcat
+
+echo"●●●●●●●●●●●●●●●● " | lolcat
+echo "..10 %" | lolcat
+sleep 2
+echo ".......30 %" | lolcat
+sleep 2
+echo ".............50 %" | lolcat
+sleep 2
+echo "....................80%" | lolcat
+sleep 2
+echo "..........................90 %" | lolcat
+sleep 2
+echo "________________________________100 %" | lolcat
+sleep 5
+
+
+echo ".[ok] .........Hacked 50 afg " | lolcat
+sleep 10
+
+
+echo ".[ok] .........Hacked 20 afg " | lolcat
+sleep 10
+
+
+echo ".[Not] ......................" | lolcat
+sleep 10
+
+
+echo ".[ok] ..........Hacked 30 afg" | lolcat
+sleep 10
+
+
+echo ".[ok] .........Hacked 50 afg" | lolcat
+sleep 10
+
+
+
+echo ".[ok] .........Hacked 100 afg" | lolcat
+
+sleep 10
+
+echo ".[ok] .........Hacked 60 afg" | lolcat
+
+sleep 10
+
+echo ".[not] ..................... " | lolcat
+
+sleep 10
+
+echo ".[ok] .........Hacked 12 afg" | lolcat
+
+sleep 10
+
+echo ".[ok] .........Hacked 500 afg" | lolcat
+
+
+sleep 10
+echo ".[ok] .........Hacked 15 afg" | lolcat
+
+sleep 10
+
+echo ".[not] ....................." | lolcat
+sleep 10
+
+
+echo ".[ok] .........Hacked 65 afg" | lolcat
+sleep 10
+
+
+echo ".[ok] .........Hacked 09 afg" | lolcat
+
+sleep 10
+
+echo ".[ok] .........Hacked 13 afg" | lolcat
+
+sleep 10
+
+echo ".[ok] .........Hacked 45 afg" | lolcat
+
+sleep 10
+
+echo ".[ok] .........Hacked 37 afg" | lolcat
+sleep 10
+
+
+echo ".[ok] .........Hacked 23 afg" | lolcat
+
+sleep 10
+
+echo ".[ok] .........Hacked 50 afg" | lolcat
+
+sleep 10
+echo ".[ok] .........Hacked 50 afg" | lolcat
+
+sleep 10
+
+echo ".[ok] .........Hacked 100 afg" | lolcat
+sleep 10
+
+
+echo ".[ok] .........Hacked 65 afg" | lolcat
+sleep 10
+
+echo ".[ok] .........Hacked 23 afg" | lolcat
+echo ".[ok] .........Hacked 50 afg " | lolcat
+sleep 10
+
+
+echo ".[ok] .........Hacked 20 afg " | lolcat
+sleep 10
+
+
+echo ".[Not] ......................" | lolcat
+sleep 10
+
+
+echo ".[ok] ..........Hacked 30 afg" | lolcat
+sleep 10
+
+
+echo ".[ok] .........Hacked 50 afg" | lolcat
+sleep 10
+
+
+
+echo ".[ok] .........Hacked 100 afg" | lolcat
+
+sleep 10
+
+echo ".[ok] .........Hacked 60 afg" | lolcat
+
+sleep 10
+
+echo ".[not] ..................... " | lolcat
+
+sleep 10
+
+echo ".[ok] .........Hacked 12 afg" | lolcat
+
+sleep 10
+
+echo ".[ok] .........Hacked 500 afg" | lolcat
+
+
+sleep 10
+echo ".[ok] .........Hacked 15 afg" | lolcat
+
+sleep 10
+
+echo ".[not] ....................." | lolcat
+sleep 10
+
+
+echo ".[ok] .........Hacked 65 afg" | lolcat
+sleep 10
+
+
+echo ".[ok] .........Hacked 09 afg" | lolcat
+
+sleep 10
+
+echo ".[ok] .........Hacked 13 afg" | lolcat
+
+sleep 10
+
+echo ".[ok] .........Hacked 45 afg" | lolcat
+
+sleep 10
+
+echo ".[ok] .........Hacked 37 afg" | lolcat
+sleep 10
+
+
+echo ".[ok] .........Hacked 23 afg" | lolcat
+
+sleep 10
+
+echo ".[ok] .........Hacked 50 afg" | lolcat
+
+sleep 10
+echo ".[ok] .........Hacked 50 afg" | lolcat
+
+sleep 10
+
+echo ".[ok] .........Hacked 100 afg" | lolcat
+sleep 10
+
+
+echo ".[ok] .........Hacked 65 afg" | lolcat
+sleep 10
+
+echo ".[ok] .........Hacked 23 afg" | lolcat
+
+sleep 
+echo "Campleted reward the all belance "
+echo " Enter youer phone number" | lolcat
+echo "+93 (0) "
+sleep 10
+echo " ✅✅ Tanks for use thes all credit is entered youer sim plese check new belanse " | lolcat
